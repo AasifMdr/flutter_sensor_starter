1a2aa2a335cfd00838c95df42e4894190f70adf9
Aasif Manandhar <aasifmanandhar09@gmail.com>
2023-07-11 11:01:49 +0545

    first commit

---
+++ lib/proximity.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:all_sensors2/all_sensors2.dart';
+import 'package:flutter/material.dart';
+
+class ProximityScreen extends StatefulWidget {
+  const ProximityScreen({super.key});
+
+  @override
+  State<ProximityScreen> createState() => _ProximityScreenState();
+}
+
+class _ProximityScreenState extends State<ProximityScreen> {
+  double _proximityValue = 0;
+  final List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(proximityEvents!.listen((ProximityEvent event) {
+      setState(() {
+        _proximityValue = event.proximity;
+      });
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Proximity'),
+      ),
+      body: const Center(
+        child: Text('Proximity'),
+      ),
+    );
+  }
+}
